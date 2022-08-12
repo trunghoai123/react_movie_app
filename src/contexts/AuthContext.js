9de4b05114272a3fe2402184ae614ca1035d3f55
trunghoai <trunghoaiitiuh@gmail.com>
2022-08-13 02:03:05 +0700

    modify a little

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -37,7 +37,7 @@ const AuthProvider = (props) => {
          } else {
             setUser(null);
          }
-         console.log(user);
+         // console.log(user);
       });
    }, []);
    return (
